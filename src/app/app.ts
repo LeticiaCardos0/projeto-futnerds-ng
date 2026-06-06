696243cead1260969819e032442bda04ee70b332
Leticia Cardoso <leticiaapc06@gmail.com>
2026-06-06 17:11:36 -0300

    add home

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Topbar } from "./pages/layout/topbar/topbar";
 import { Footer } from "./pages/layout/footer/footer";
+import { Home } from "./pages/home/home";
 
 
 @Component({
   selector: 'app-root',
-  imports: [Topbar, Footer],
+  imports: [Topbar, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
